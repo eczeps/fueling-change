b347b1d358f964bcd824e9ecb70373aba3c7431c
Estrella <egarcia2@cs.wellesley.edu>
2019-11-05 19:23:21 -0500

    updated sql tables

---
+++ draft/database.sql
@@ -0,0 +1,49 @@
+-- this file sets up the 'database' for the 'Fueling-Change App'
+-- The database consists of tables: 
+
+drop table if exists userform;
+drop table if exists completed;
+drop table if exists starred;
+drop table if exists user;
+drop table if exists achievement;
+
+Create table user(
+	UID int auto_increment,
+	first_Name varchar(30),
+	last_Name varchar(30),
+	footprint float,
+	username varchar(30),
+	password varchar(30),
+	Primary key (UID)
+);
+
+create table achievement(
+	AID int auto_increment,
+	title varchar(30),
+	description varchar(50),
+	isRepeatable int(1),
+	isSelfReport int(1),
+	Primary Key (AID)
+);
+
+create table userform(
+    UID int,
+	Primary key (UID),
+	flights int,
+	driving int,
+	meat int,
+	laundry int
+);
+
+create table completed(
+    UID int,
+    AID int,
+	Primary Key (UID, AID),
+	timestamp datetime
+);
+
+create table starred(
+    UID int,
+    AID int,
+	Primary Key (UID, AID)
+);
