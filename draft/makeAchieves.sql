e8383dd1df543f8d2424513d3c54adb92bd58d9f
Estrella <egarcia2@cs.wellesley.edu>
2019-11-17 16:49:13 -0500

    added achievements

--- draft/makeAchieves.sql
+++ draft/makeAchieves.sql
@@ -1,8 +1,26 @@
 use egarcia2_db;
 
 -- Creating achievements
+insert into achievement(title,description)
+values ('Joined Fuelling Change!','Started their journey to lower carbon footprint');
+
 insert into achievement(title,description,isRepeatable,isSelfReport)
 values ('No Meat for a Week','Do not eat meat for seven days.',1,1); -- 1 is true, 0 is false
+
+insert into achievement(title,description,isRepeatable,isSelfReport)
+values ('Carpooled','Carpooled to Work/School.',1,1);
+
+insert into achievement(title,description,isRepeatable,isSelfReport)
+values ('Vegan','Became Vegan!',1,1);
+
+insert into achievement(title,description,isRepeatable,isSelfReport)
+values ('Vegetarian','Became Vegetarian',1,1);
+
+insert into achievement(title,description,isRepeatable,isSelfReport)
+values ('Earthly Drying','Line-Dried Clothing',1,1);
+
+insert into achievement(title,description,isRepeatable,isSelfReport)
+values ('+1 Tree','Planted A Tree',1,1);
 
 insert into achievement(title,description,isSelfReport)
 values ('Electric Car 1','Drive an electric car.',1); -- 0 is default
