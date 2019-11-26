062098ea773d1833917b2df6cb1e97b25ba8d4d2
Alissa <atinney@wellesley.edu>
2019-11-26 13:42:53 -0500

    updated comments on database.sql

--- draft/database.sql
+++ draft/database.sql
@@ -5,6 +5,8 @@ drop table if exists starred;
 drop table if exists user;
 drop table if exists achievement;
 
+/* User table will hold all of the user's information
+Used to calculate the user's carbon footprint, as well as personal info */
 Create table user(
 	UID int auto_increment,
 	first_Name varchar(30),
@@ -26,6 +28,8 @@ Create table user(
 	Primary key (UID)
 );
 
+/* achievement table stores information
+about all possible achievements a user can earn */
 create table achievement(
 	AID int auto_increment,
 	title varchar(30),
@@ -37,6 +41,9 @@ create table achievement(
 	Primary Key (AID)
 );
 
+/* achievement table will hold userIDs and achievement IDs as key value
+pairs to indicate which users have completed what. the counter indicates
+how many time a user has completed the achievement */
 create table completed(
     UID int,
     AID int,
@@ -49,6 +56,9 @@ create table completed(
         on delete cascade
 );
 
+/* similar to the completed table, the starred table userIDs
+and achievement IDs as key value pairs to indicate which users
+certain users have starred certain achievements */
 create table starred(
     UID int,
     AID int,
