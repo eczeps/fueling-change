2b55a919aad53c2daf50602d1d40cdb4394c3fad
Alissa <atinney@wellesley.edu>
2019-12-08 22:35:36 -0500

    added another user to webpageTest

--- draft/webpageTest.sql
+++ draft/webpageTest.sql
@@ -40,6 +40,12 @@ insert into user(first_Name,last_Name,footprint,username,password,
                 servings_turkey,laundry)
 values ('estrella','garcia',5000.0,'egarcia2','cba',50,32,2,3,2,1,5,3,2);
 
+insert into user(first_Name,last_Name,footprint,username,password,
+                miles_flown,miles_driven,servings_lamb,servings_beef,
+                servings_cheese,servings_pork,servings_chicken,
+                servings_turkey,laundry)
+values ('alissa','tinney',5000.0,'atinney','pass',50,32,2,3,2,1,5,3,2);
+
 -- Inserting into completed
 insert into completed(UID,AID)
 values(2,1);
@@ -50,6 +56,15 @@ values(2,7);
 insert into completed(UID,AID)
 values(2,10);
 
+insert into completed(UID,AID)
+values(1,1);
+
+insert into completed(UID,AID)
+values(1,7);
+
+insert into completed(UID,AID)
+values(1,10);
+
 -- Inserting into starred
 insert into starred(UID,AID)
 values(2,2);
@@ -59,3 +74,12 @@ values(2,5);
 
 insert into starred(UID,AID)
 values(2,8);
+
+insert into starred(UID,AID)
+values(1,2);
+
+insert into starred(UID,AID)
+values(1,5);
+
+insert into starred(UID,AID)
+values(1,8);
